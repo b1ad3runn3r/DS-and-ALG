d743b7ff92e57e84ef502e9be51952a2068cc6d3
b1ad3runn3r <31962327+b1ad3runn3r@users.noreply.github.com>
2023-03-28 10:59:19 +0300

    lab3: b: initial commit Copy source tree from lab3.a.

---
+++ lab3/b/src/include/dialog.c
@@ -0,0 +1,178 @@
+#include "dialog.h"
+#include "utils.h"
+#include "types.h"
+#include "table.h"
+#include <stdio.h>
+#include <string.h>
+
+void print_opts(const char *opts[], size_t size) {
+    for (int i = 0; i < size; ++i) {
+        printf("%s\n", opts[i]);
+    }
+}
+
+int dialog(int opts_size) {
+    int choice = 0;
+    int status = 0;
+
+    do {
+        status = get_int("Enter option: ", &choice);
+        if (status != E_OK) {
+            printf("Wrong input! Try again.\n");
+        }
+    } while (choice < 0 || choice >= opts_size || status != E_OK);
+
+    return choice;
+}
+
+static KeySpace *create_element(char* key, char* par, int data) {
+    if (!key){
+        return NULL;
+    }
+
+    KeySpace *element = calloc(1, sizeof(KeySpace));
+    if (!element) {
+        return NULL;
+    }
+
+    Item *item = calloc(1, sizeof(Item));
+    if (!item) {
+        free(element);
+        return NULL;
+    }
+
+    item->info = calloc(1, sizeof(InfoType));
+    if (!item->info) {
+        free(item);
+        free(element);
+        return NULL;
+    }
+
+    *(item->info) = data;
+    element->key = key;
+    element->par = par;
+    element->info = item;
+
+    return element;
+}
+
+int d_insert(Table *table) {
+    char *key = readline("Enter key: ");
+    if (!key) {
+        return E_ALLOC;
+    }
+
+    char *par = readline("Enter parent key: ");
+    if (!par) {
+        free(key);
+        return E_ALLOC;
+    }
+    if (par[0] == '\0') {
+        free(par);
+        par = NULL;
+    }
+
+    int data = 0;
+    if (get_int("Enter data: ", &data) != E_OK) {
+        free(key);
+        free(par);
+        return E_WRONGINPUT;
+    }
+
+    KeySpace *element = create_element(key, par, data);
+    if (!element) {
+        free(key);
+        free(par);
+        return E_WRONGINPUT;
+    }
+
+    int status = insert(table, element);
+    if (status != E_OK) {
+        free_element(element);
+    }
+    parse_result(status);
+    free(element);
+
+    return E_OK;
+}
+
+int d_remove(Table *table) {
+    char *key = readline("Enter key: ");
+    if (!key) {
+        return E_WRONGINPUT;
+    }
+
+    KeySpace *element = create_element(key, NULL, 0);
+    int status = E_ALLOC;
+
+    if (element) {
+        status = remove_element(table, element);
+    }
+
+    parse_result(status);
+
+    free_element(element);
+    free(element);
+
+    return E_OK;
+}
+
+int d_search(Table *table) {
+    char *key = readline("Enter key: ");
+    if (!key) {
+        return E_WRONGINPUT;
+    }
+    KeySpace *element = create_element(key, NULL, 0);
+    if (element) {
+        IndexType found = search(table, element, 0);
+        if (found != E_NOTFOUND) {
+            printf("Found: %d\n", found);
+            print_element(table->ks + found);
+        }
+        else {
+            parse_result(E_NOTFOUND);
+        }
+    }
+    free_element(element);
+    free(element);
+    return E_OK;
+}
+
+int d_print(Table *table) {
+    print_table(table);
+    return E_OK;
+}
+
+int d_garbage(Table *table) {
+    remove_garbage(table);
+    return E_OK;
+}
+
+void parse_result(int result) {
+    switch (result) {
+        case E_OK:
+            break;
+        case E_ALLOC:
+            printf ("Memory allocation error!\n");
+            break;
+        case E_WRONGINPUT:
+            printf ("Wrong input!\n");
+            break;
+        case E_NOTFOUND:
+            printf ("Not found!\n");
+            break;
+        case E_INSERT:
+            printf ("Insert error!\n");
+            break;
+        case E_OVERFLOW:
+            printf ("Overflow!\n");
+            break;
+        case E_NULLPTR:
+            printf ("Null pointer!\n");
+            break;
+        default:
+            printf ("Unknown error!\n");
+            break;
+    }
+}
+
---
+++ lab3/b/src/include/table.c
@@ -0,0 +1,193 @@
+#include "table.h"
+#include "types.h"
+#include <stdio.h>
+#include <string.h>
+
+static inline int compare_keys(const KeyType *k1, const KeyType *k2) {
+    return strcmp(k1, k2);
+}
+
+Table *init_table(IndexType msize) {
+    if (msize <= 0) {
+        return NULL;
+    }
+
+    Table *table = calloc(1, sizeof(Table));
+    if (!table) {
+        return NULL;
+    }
+
+    table->msize = msize;
+    table->csize = 0;
+    table->ks = NULL;
+
+    return table;
+}
+
+void print_element(const KeySpace *element) {
+    if (!element || !element->key) {
+        return ;
+    }
+
+    printf( 
+            ELEM_FMT, 
+            element->busy, 
+            element->key, 
+            element->par, 
+            *(element->info->info)
+        );
+}
+
+void print_table(const Table *table) {
+    printf("Busy\tKey\tPar\tInfo\n");
+    for (IndexType i = 0; i < table->csize; ++i) {
+        print_element(table->ks + i);
+    }
+}
+
+void free_element(KeySpace *element) {
+    if (!element || !element->key){
+        return ;
+    }
+        
+    free(element->info->info);
+    free(element->info);
+    free(element->key);
+    free(element->par);
+}
+
+void free_table(Table *table) {
+    if (!table) {
+        return ;
+    }
+
+    for (IndexType i = 0; i < table->csize; ++i) {
+        free_element(table->ks + i);
+    }
+
+    if (table->csize > 0) {
+        free(table->ks);
+    }
+    free(table);
+}
+
+int remove_garbage(Table *table) {
+    if (!table) {
+        return E_NULLPTR;
+    }
+
+    for (IndexType i = 0; i < table->csize; ++i) {
+        if ((table->ks + i)->busy == 0) {
+            free_element(table->ks + i);
+            memmove(table->ks + i, table->ks + i + 1, (table->csize - i - 1) * sizeof(KeySpace));
+            --table->csize;
+            --i;
+        }
+    }
+
+    if (table->msize == table->csize) {
+        return E_OVERFLOW;
+    }
+    else {
+        KeySpace *tmp_ptr = NULL;
+        tmp_ptr = realloc(table->ks, table->csize * sizeof(KeySpace));
+
+        if (!tmp_ptr) {
+            return E_ALLOC;
+        }
+
+        table->ks = tmp_ptr;
+        return E_OK;
+    }
+}
+
+IndexType search(const Table *table, const KeySpace *element, int parent) {
+    if (!table || !element) {
+        return E_ALLOC;
+    }
+
+    if (parent && !(element->par)) {
+        return E_NULLPTR;
+    }
+
+    for (IndexType i = 0; i < table->csize; ++i) {
+        if (parent) {
+            if ((table->ks + i)->busy && !compare_keys((table->ks + i)->key, element->par)) {
+                return i;
+            }
+        }
+        else {
+            if ((table->ks + i)->busy && !compare_keys((table->ks + i)->key, element->key)) {
+                return i;
+            }
+        }
+    }
+
+    return E_NOTFOUND;
+}
+
+int remove_element(Table *table, const KeySpace *element) {
+    if (!table || !element) {
+        return E_NULLPTR;
+    }
+
+    int idx = search(table, element, 0);
+    if (idx == E_NOTFOUND) {
+        return E_NOTFOUND;
+    }
+    KeySpace *cur_elem = table->ks + idx;
+    cur_elem->busy = 0;
+
+    for (IndexType i = 0; i < table->csize; ++i) {
+        if ((table->ks + i)->par) {
+            if (!compare_keys(cur_elem->key, (table->ks + i)->par)) {
+                remove_element(table, table->ks + i);
+            }
+        }
+    }
+
+    return E_OK;
+}
+
+int insert(Table *table, const KeySpace *element) {
+    if (!table || !element) {
+        return E_NULLPTR;
+    }
+
+    if (element->par) {
+        if (search(table, element, 1) == E_NOTFOUND) {
+            return E_INSERT;
+        }
+    }
+
+    if (table->csize == table->msize) {
+        if (remove_garbage(table) != E_OK) {
+            if (table->csize > 0) {
+                return E_OVERFLOW;
+            }
+        }
+    }
+
+    if (search(table, element, 0) != E_NOTFOUND) {
+        return E_INSERT;
+    }
+
+    KeySpace *tmp_ptr = NULL;
+    if (table->csize == 0) {
+        tmp_ptr = calloc(1, sizeof(KeySpace));
+    }
+    else {
+        tmp_ptr = realloc(table->ks, (table->csize + 1) * sizeof(KeySpace));
+    }
+
+    if (!tmp_ptr) {
+        return E_ALLOC;
+    }
+
+    table->ks = tmp_ptr;
+    *(table->ks + table->csize) = *element;
+    (table->ks + table->csize)->busy = 1;
+    table->csize += 1;
+
+    return E_OK;
+}
