fab27070f522e4947cbf5ac4fd5abc312cd97204
b1ad3runn3r <31962327+b1ad3runn3r@users.noreply.github.com>
2023-05-23 02:38:01 +0200

    lab5: add utils functions

---
+++ lab5/src/include/utils.h
@@ -0,0 +1,14 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <stdlib.h>
+#include <stdint.h>
+
+void clear_screen();
+char *readline(const char* prompt);
+int get_int(const char *prompt, int *res);
+int get_size_t(const char *prompt, size_t *res);
+int get_int8_t(const char *prompt, int8_t *res);
+
+
+#endif // UTILS_H
